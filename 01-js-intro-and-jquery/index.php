f52361b40a54bc065a1846fb339342ec405f41fe
Gaston Mira <gastonmira@gmail.com>
2012-11-14 03:27:18 -0300

    Topic 1 almost finished. I couldn ´t do point number 10.

--- 01-js-intro-and-jquery/index.php
+++ 01-js-intro-and-jquery/index.php
@@ -25,11 +25,39 @@
  					error: function (response, status, xhr) {
  							$('div.fail')
  								.html(status+ ' - '+ xhr.status);
- 					},
+ 					}
 
 
  				});
  			}
+
+ 				function doMovies(){
+ 					
+ 					$.ajax({
+ 						type: 'GET',
+ 						url: 'http://bootcamp.aws.af.cm/movies',
+ 						dataType:'json',
+ 						crossDomain: true,
+ 						
+ 						success: function (data){
+ 							var array= data.d.results;
+ 							//$.each(array, function() { with function $.each I couldn´t do it.
+ 							for (var i = 0 ; i < array.length ; i ++ ) {  								
+ 								$('div.movies').append('</br></br><b>Name: </b></br>'+ array[i].Name +  '</br><b> Synopsis: </b> </br> '    + array[i].Synopsis + '</br><b>Year:  </b></br>' + array[i].ReleaseYear + '</br><b>Box Art: </b></br> <img src="' + array[i].BoxArt.MediumUrl +'"></img>' );
+ 							}
+		 				//});
+		 				},
+ 						error: function (response, status, xhr) {
+ 							$('div.fail')
+ 								.html(status+ ' - '+ xhr.status);
+ 						}
+
+
+
+ 					}); 						
+ 				}
+ 			
+ 			
  		</script> 
  		<title>Globant :: Welcome to the HTML5 bootcamp</title> 
  		<style type="text/css">
@@ -42,7 +70,9 @@
  				color: white;
  			}
 
-
+ 			.movies {
+	 			background: -webkit-linear-gradient(rgba(58,112,206,0.88) 95.03%, #ffffff 100%) /* sorry for this color haha */
+ 			}
  		</style>                                                            
  	</head>                                                                 
  	
@@ -53,5 +83,7 @@
  	  	<script>document.getElementById('alias').focus()</script>
  	  	<div class="response"></div>
  	  	<div class="fail"></div>
+ 	  	<button onclick="javascript:doMovies();">List of Movies!</button> 
+ 	   	<div class="movies" > </div>
  	</body>                                                                 
  </html>
